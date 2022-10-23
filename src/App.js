906047c388cd7d02f39a931a5643ba41b2b9f5ee
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2022-10-23 22:23:18 +0600

    using mapStateToProps and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { useSelector, useDispatch } from "react-redux";
 import "./styles.css";
 import { incrementCount, decrementCount, resetCount } from "./state/actions";
+import { connect } from "react-redux";
 
-export default function App() {
-  const { count } = useSelector((state) => state);
+function App({ count, incrementCount }) {
+  // const { count } = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Counter App</h1>
       <h2>{count}</h2>
-      <button onClick={() => dispatch(incrementCount())}>increment</button>
+      <button onClick={incrementCount}>increment</button>
       <button onClick={() => dispatch(decrementCount())}>decrement</button>
       <button onClick={() => dispatch(resetCount())}>reset</button>
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    incrementCount: () => dispatch(incrementCount())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
